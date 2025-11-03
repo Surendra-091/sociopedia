d744368becea38a6cc0d14a1010dfda7b9c3080a
Surendra-091 <surendra.22bce20386@vitapstudent.ac.in>
2025-11-04 00:37:06 +0530

    Update axios base URL for API requests

--- frontend/src/Utils/axiosInstance.js
+++ frontend/src/Utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://sociopedia-front-t7ge.onrender.com/api", // backend URL
+  baseURL: "https://sociopedia-backend-fajy.onrender.com/api", // backend URL
 });
 
 // Automatically attach token to every request if available
